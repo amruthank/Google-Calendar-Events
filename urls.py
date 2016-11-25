7a2943314ecf4d24085d8bccbf1db61d0d98d7dd
Amrutha NK <amruthank196@gmail.com>
2016-11-25 16:40:25 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -7,9 +7,9 @@ urlpatterns = patterns('',
         # example: /createCalenderEvent
         url(r'^createCalenderEvent/$', views.createCalenderEvent, name='createCalenderEvent'),
         # example: /editCalendarEvent
-        url(r'^editCalendarEvent/$', views.editCalendarEvent, name='editCalendarEvent'),
+        url(r'^editCalendarEvent/(?P<event_id>\S+)/$', views.editCalendarEvent, name='editCalendarEvent'),
         # example: /deleteCalendarEvent
-        url(r'^deleteCalendarEvent/$', views.deleteCalendarEvent, name='deleteCalendarEvent'),
+        url(r'^deleteCalendarEvent/(?P<event_id>\S+)/$', views.deleteCalendarEvent, name='deleteCalendarEvent'),
         # example: /showMyEvents
         url(r'^showMyEvents/$', views.showMyEvents, name='showMyEvents'),
 )
